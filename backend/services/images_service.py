0ea7154f4eeb7ed9876d668adbc375b9bad455df
Giovanna Albuquerque <107943642+gioalbuquerque@users.noreply.github.com>
2026-05-05 20:57:51 -0300

    Criação do service de processamento de imagem com api do open router

---
+++ backend/services/images_service.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+import base64
+from prompts.prompts import TEMPLATE_GLICEMIA_PROMPT
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+def process_image_service(imagem_b64, status):
+    try:
+        response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json",
+        },
+        data=json.dumps({
+            "model": "nvidia/nemotron-nano-12b-v2-vl:free",
+            "messages": [
+            {
+                "role": "user",
+                "content": [
+                {
+                    "type": "text",
+                    "text": TEMPLATE_GLICEMIA_PROMPT
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                    "url": f"data:image/jpeg;base64,{imagem_b64}"
+                    }
+                }
+                ]
+            }
+            ],
+            "reasoning": {"enabled": False}
+        })
+        )
+
+        response = response.json()
+        response = response['choices'][0]['message']['content']
+        return response, 200
+    except:
+        return "Erro ao chamar LLM", 500
