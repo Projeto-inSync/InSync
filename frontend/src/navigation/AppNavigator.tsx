70731190c340a7f1f1c97562f477297b3601e5b4
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2026-05-24 19:29:28 -0300

    ajusta rotas do aplicativo

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// 1. Importação de todas as nossas telas
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 // import ProfileSelectionScreen from '../screens/ProfileSelectionScreen';
-import ChildRegisterScreen from '../screens/ChildRegisterScreen';
+// import ChildRegisterScreen from '../screens/ChildRegisterScreen';
 import ResponsibleRegisterScreen from '../screens/ResponsibleRegisterScreen';
 import AnalysisScreen from '../screens/AnalysisScreen';
-import FoodResultScreen from '../screens/FoodResultScreen'; // <-- A tela que estava faltando no mapa!
+import FoodResultScreen from '../screens/FoodResultScreen';
 import PaymentScreen from '../screens/PaymentScreen';
 import AdminScreen from '../screens/AdminScreen';
 import TabNavigator from './TabNavigator';
@@ -25,17 +23,10 @@ export default function AppNavigator() {
         initialRouteName="Login"
         screenOptions={{ headerShown: false }}
       >
-        {/* Fluxo de Autenticação */}
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        {/* <Stack.Screen name="ProfileSelection" component={ProfileSelectionScreen} /> */}
-        <Stack.Screen name="ChildRegister" component={ChildRegisterScreen} />
         <Stack.Screen name="ResponsibleRegister" component={ResponsibleRegisterScreen} />
-        
-        {/* O Menu Principal do App */}
         <Stack.Screen name="HomeTab" component={TabNavigator} />
-
-        {/* Telas de Gamificação (Sobrepostas ao Menu) */}
         <Stack.Screen name="Analysis" component={AnalysisScreen} />
         <Stack.Screen name="FoodResult" component={FoodResultScreen} />
         <Stack.Screen name="Payment" component={PaymentScreen} />
