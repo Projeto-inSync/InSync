1423e9b1b9cbd60a5db37d95512ab706ced8a534
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2026-05-24 22:26:05 -0300

    ajusta navegação

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -3,15 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
-// import ProfileSelectionScreen from '../screens/ProfileSelectionScreen';
-// import ChildRegisterScreen from '../screens/ChildRegisterScreen';
-import ResponsibleRegisterScreen from '../screens/ResponsibleRegisterScreen';
 import AnalysisScreen from '../screens/AnalysisScreen';
 import FoodResultScreen from '../screens/FoodResultScreen';
 import PaymentScreen from '../screens/PaymentScreen';
 import AdminScreen from '../screens/AdminScreen';
 import TabNavigator from './TabNavigator';
 import AddDependentScreen from '../screens/AddDependentScreen';
+import RecuperarSenha from '../screens/RecuperarSenha';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,13 +23,13 @@ export default function AppNavigator() {
       >
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="ResponsibleRegister" component={ResponsibleRegisterScreen} />
         <Stack.Screen name="HomeTab" component={TabNavigator} />
         <Stack.Screen name="Analysis" component={AnalysisScreen} />
         <Stack.Screen name="FoodResult" component={FoodResultScreen} />
         <Stack.Screen name="Payment" component={PaymentScreen} />
         <Stack.Screen name="Admin" component={AdminScreen} />
         <Stack.Screen name="AddDependent" component={AddDependentScreen} />
+        <Stack.Screen name="RecuperarSenha" component={RecuperarSenha} />
         
       </Stack.Navigator>
     </NavigationContainer>
