7b880d6bde3b3baa6861ee1c1f86257c76b2beed
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2026-06-15 23:54:34 -0300

    atualiza arquivo

--- frontend/src/theme/colors.ts
+++ frontend/src/theme/colors.ts
@@ -4,8 +4,7 @@ export const colors = {
   secondaryBlue: '#51D1E6',
   backgroundWhite: '#FFFFFF',
   black: '#000000',
-  // Adicionei algumas cores extras que são úteis para textos e fundos
   textDark: '#333333', 
   textGray: '#888888',
-  appBackground: '#F4F9F4', // Um fundo levemente esverdeado
+  appBackground: '#F4F9F4',
 };
