47f9f239828ec92ea4a0127c81dd6afbfdc57099
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2026-06-15 23:55:45 -0300

    atualiza arquivo

--- frontend/src/utils/SoundManager.tsx
+++ frontend/src/utils/SoundManager.tsx
@@ -1,7 +1,5 @@
-// Essa variável começa como true (ligada) por padrão
 export let isSoundEnabled = true;
 
-// Essa função será chamada pelo botão na tela de configurações
 export const toggleSoundEffects = (enabled: boolean) => {
   isSoundEnabled = enabled;
 };
