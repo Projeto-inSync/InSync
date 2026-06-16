61c2b197a18cd5700748d9e7b2bd76d788436b79
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2026-06-16 00:06:35 -0300

    adiciona categorias de alimentos saudáveis

---
+++ backend/services/constants.py
@@ -0,0 +1,10 @@
+CATEGORIAS_SAUDAVEIS = {
+    "carbohydrates", "carbs", "fruits", "fruit",
+    "vegetables", "veggies", "dairy", "milk",
+    "proteins", "protein", "meat", "legumes", "nuts",
+    "grains", "grain", "whole grains", "fish", "seafood",
+    "eggs", "egg", "beans", "lentils", "seeds",
+    "salad", "soup", "rice", "oats", "yogurt"
+}
+
+LIMITE_GLICEMIA_ALERTA = 40
