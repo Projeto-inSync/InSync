4cb78d505f861be4a83d738d8679e05361a28f36
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2026-06-15 23:55:32 -0300

    atualiza arquivo

--- frontend/src/utils/MusicPlayer.tsx
+++ frontend/src/utils/MusicPlayer.tsx
@@ -1,23 +1,20 @@
 import { Audio } from 'expo-av';
 
-// Variável que vai guardar a nossa música viva em todo o app
 let backgroundMusic: Audio.Sound | null = null;
 
 export const toggleBackgroundMusic = async (play: boolean) => {
   try {
-    // Se o usuário quer tocar e a música ainda não foi carregada na memória
     if (play && !backgroundMusic) {
       const { sound } = await Audio.Sound.createAsync(
-        require('../assets/trilha_insync.mp3'), // Coloque sua música aqui!
+        require('../assets/trilha_insync.mp3'),
         {
-          isLooping: true, // Aqui está a mágica do loop infinito!
-          volume: 0.3,     // Volume em 30% para ficar como som ambiente suave
+          isLooping: true,
+          volume: 0.3,
         }
       );
       backgroundMusic = sound;
     }
 
-    // Se a música já existe na memória, apenas controlamos o Play e o Pause
     if (backgroundMusic) {
       if (play) {
         await backgroundMusic.playAsync();
